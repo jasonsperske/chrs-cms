6cee9d603c32d246409d9ccfc0b10896173b51f4
Jason Sperske <jason@sperske.com>
2024-09-12 00:53:53 -0700

    added a pure vision based implemenation

--- src/app/api/openai/vision/route.ts
+++ src/app/api/openai/vision/route.ts
@@ -1,13 +1,52 @@
-import { NextResponse } from 'next/server'
-import OpenAI from 'openai';
-import * as Sharp from 'sharp'
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+import Sharp from "sharp";
 
 const openai = new OpenAI({
-    apiKey: process.env['OPENAI_API_KEY']
+  apiKey: process.env["OPENAI_API_KEY"],
 });
 
 export async function POST(request: Request) {
-    const assistant = await openai.beta.assistants.retrieve(process.env['OPENAI_VISION_ASSISTANT']!)
+  const files = await request.formData();
 
-    return NextResponse.json({ success: false, response: 'OpenAI responded but failed to parse' })
+  const images = await Promise.all(
+    Array.from(files.values()).map((field) =>
+      (field as File)
+        .arrayBuffer()
+        .then((buffer) =>
+          Sharp(buffer).resize(1024, 1024, { fit: "inside" }).jpeg().toBuffer()
+        )
+    )
+  );
+
+  function toDataURL(buffer: Buffer) {
+    return "data:image/jpg;base64," + buffer.toString("base64");
+  }
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          'I have pictures of a book or possibly a magazine. Respond in the form of a JSON. I would like you to tell me what type of media it is in a field called "mediaType" with "book" for a book and "magazine" for magazine. Tell me the title (if available) in field called "title", the author (if available) in a field called "author". If the media is a book tell me the year it was initially published if available. If the media is a magazine tell me the year it was issued in a field called "yearPublished" and the month in a field called "monthPublished" if it can be determined. If its possible tell me the name of the publisher in a field called "publisher". If it is possible identify the ISBN number (also sometimes labeled "International Standard Book No.") in a field called "serialNumber". If it is possible identify the "Library of Congress Catalog Card Number" number (also sometimes labeled "Library of Congress Catalog Number") in a field called "catalogNumber"  There may be more than one interpretations, give me these multiple interpretations with a confidence score between 0 and 1.',
+      },
+      {
+        role: "user",
+        content: images.map((image) => ({
+          type: "image_url",
+          image_url: {
+            url: toDataURL(image),
+            detail: "auto",
+          },
+        })),
+      },
+    ],
+    max_tokens: 10000,
+  });
+
+  return NextResponse.json({
+    success: response.choices.length > 0,
+    response,
+  });
 }
--- src/components/MultipleImageInput.tsx
+++ src/components/MultipleImageInput.tsx
@@ -1,28 +1,140 @@
-'use client'
-import { FormEvent, useState } from "react"
-import { BookVariation } from "./ImageOCRInput"
-import { Button } from "./ui/button"
+"use client";
+import React, { FormEvent, useState } from "react";
+import { BookVariation } from "./ImageOCRInput";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardHeader } from "./ui/card";
 
 type Props = {
-    onSelectVariant: (variant: BookVariation) => void
-}
+  onSelectVariant: (variant: BookVariation) => void;
+};
 
 export default function MultipleImageInput({ onSelectVariant }: Props) {
-    const [receivingFiles, setReceivingFiles] = useState(false)
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [variations, setVariations] = useState<BookVariation[]>([]);
+  const [files, setFiles] = useState<File[]>([]);
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
-        throw new Error("Function not implemented.")
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    setIsProcessing(true);
+    setVariations([]);
+    const formData = new FormData();
+    files.forEach((file) => formData.append("files", file));
+    fetch("/api/openai/vision", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.success) {
+          throw new Error("Failed to process images");
+        }
+        const payload = data.response.choices[0].message.content;
+        if (payload.startsWith("```json") && payload.endsWith("```")) {
+          const ai = JSON.parse(payload.slice(7, -3));
+          if (ai.length > 0) {
+            setVariations(ai);
+          } else if (
+            ai["interpretations"] &&
+            ai["interpretations"].length > 0
+          ) {
+            setVariations(ai.interpretations);
+          } else if (ai["confidence"]) {
+            setVariations([ai]);
+          } else {
+            throw new Error("Failed to find any matches");
+          }
+        } else {
+          throw new Error("Failed to parse response");
+        }
+      })
+      .finally(() => setIsProcessing(false));
+  }
+
+  function handleDrop(event: React.DragEvent<HTMLDivElement>): void {
+    event.preventDefault();
+    if (event.dataTransfer.items) {
+      setFiles([
+        ...files,
+        ...Array.from(event.dataTransfer.items)
+          .filter(
+            (item) => item.kind === "file" && item.type.startsWith("image/")
+          )
+          .map((item) => item.getAsFile())
+          .filter((x) => x !== null),
+      ]);
     }
+  }
 
-    function handleDragEnter() {
-        setReceivingFiles(true)
-    }
+  function handleDragOver(event: React.DragEvent<HTMLDivElement>): void {
+    event.preventDefault();
+  }
 
-    return (
-        <form method="POST" action="/api/openai/vision" encType="multipart/form-data" onDragEnter={handleDragEnter} onSubmit={handleSubmit}>
-            <div className="border border-solid border-neutral-600 rounded-sm">
-                <div hidden={!receivingFiles}>Drop images here</div>
-            </div>
-            <Button>Submit</Button>
-        </form>)
+  return (
+    <>
+      <form
+        method="POST"
+        action="/api/openai/vision"
+        encType="multipart/form-data"
+        onSubmit={handleSubmit}
+        className="flex gap-2"
+      >
+        <div
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+          className="border border-dashed border-neutral-600 rounded-sm h-22 w-22 grow p-2 gap-2"
+        >
+          <div hidden={files.length > 0}>Drop images here</div>
+          <div hidden={!isProcessing}>Analyzing...</div>
+          <div hidden={isProcessing} className="flex flex-wrap gap-4">
+            {files.map((file, index) => (
+              <div key={index} className="relative">
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt={file.name}
+                  className="h-20 w-20 object-cover rounded-xl"
+                />
+                <Button
+                  variant="ghost"
+                  size={"icon"}
+                  onClick={() => setFiles(files.filter((_, i) => i !== index))}
+                  className="absolute top-0 right-0 p-1 bg-transparent text-white rounded-full"
+                >
+                  &times;
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Button
+          disabled={files.length == 0 || isProcessing}
+          className="flex-none"
+        >
+          Submit
+        </Button>
+      </form>
+      {variations.map((variant, index) => (
+        <Card
+          key={`variant-${index}`}
+          className="p-2 max-w-sm cursor-pointer"
+          onClick={() => {
+            onSelectVariant(variant);
+            setFiles([]);
+            setVariations([]);
+          }}
+        >
+          <CardHeader>{variant.title}</CardHeader>
+          <CardContent>
+            {variant.author ? <p>by {variant.author}</p> : null}
+            <p>
+              Published by {variant.publisher}{" "}
+              {variant.yearPublished > 1800 ? `(${variant.yearPublished})` : ""}
+            </p>
+          </CardContent>
+          <CardDescription>
+            confidence {Math.floor(variant.confidence * 100)}%
+          </CardDescription>
+        </Card>
+      ))}
+    </>
+  );
 }
